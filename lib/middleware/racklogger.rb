da77454e227a5cce98aa1b759562136cd140bc7a
Tim Arbaev <timbrk@gmail.com>
2019-03-28 18:51:00 +0300

    using Logger

--- lib/middleware/racklogger.rb
+++ lib/middleware/racklogger.rb
@@ -1,18 +1,22 @@
+require 'logger'
+
 class RackLogger
   LOG_DIRNAME = "log".freeze
   LOG_FILENAME = "app.log".freeze
 
   def initialize(app)
     @app = app
-    Dir.mkdir("#{Simpler.root}/#{LOG_DIRNAME}") unless Dir.exist?("#{Simpler.root}/#{LOG_DIRNAME}")
+
+    file = File.open("#{Simpler.root}/#{LOG_DIRNAME}/#{LOG_FILENAME}", "a+")
+    @logger = Logger.new(file, formatter: proc {|severity, datetime, progname, msg|
+      "#{datetime}#{msg}\n"
+    })
   end
 
   def call(env)
-    start = Time.now
     status, headers, body = @app.call(env)
 
-    @data = {
-        start: start,
+    data = {
         status: status,
         headers: headers,
         verb: env['REQUEST_METHOD'],
@@ -21,55 +25,51 @@ class RackLogger
         action: env['simpler.action'],
         params: env['simpler.params'],
         template: env['simpler.template'],
-        stop: Time.now
     }
 
-    File.open("#{Simpler.root}/#{LOG_DIRNAME}/#{LOG_FILENAME}", "a+") {|f| f.write(output) }
+    log_write(data)
 
     [status, headers, body]
   end
 
   private
 
-  def output
-    "#{show_divider}\n#{show_time}\n#{show_request}\n#{show_handler}\n#{show_params}\n#{show_response}\n"
+  def log_write(data)
+    @logger.info(output(data))
+    @logger.close
   end
 
-  def show_time
-    elapsed_time = @data[:stop] - @data[:start]
-    "#{@data[:start].strftime "%Y-%m-%d %H:%M:%S"}, response time: #{to_ms(elapsed_time)}ms"
+  def output(data)
+    <<~LOG_RECORD
+      \n#{show_request(data)}
+      #{show_handler(data)}
+      #{show_params(data)}
+      #{show_response(data)}
+    LOG_RECORD
   end
 
-  def to_ms(seconds)
-    (seconds * 1000.0).round(5)
+  def show_request(data)
+    "Request: #{data[:verb]} #{data[:uri]}"
   end
 
-  def show_request
-    "Request: #{@data[:verb]} #{@data[:uri]}"
-  end
-
-  def show_handler
-    handler = @data[:controller].nil? ? "N/A" : "#{@data[:controller].class}##{@data[:action]}"
+  def show_handler(data)
+    handler = data[:controller].nil? ? "N/A" : "#{data[:controller].class}##{data[:action]}"
     "Handler: #{handler}"
   end
 
-  def show_params
-    parameters = @data[:params].nil? || @data[:params].empty? ? "none" : @data[:params]
+  def show_params(data)
+    parameters = data[:params].nil? || data[:params].empty? ? "none" : data[:params]
     "Parameters: #{parameters}"
   end
 
-  def show_response
-    show_status = "#{@data[:status]} #{Rack::Utils::HTTP_STATUS_CODES[@data[:status]]}"
-    show_header = "[#{@data[:headers]['Content-Type']}]"
-    "Response: #{show_status}, #{show_header} #{show_template}"
+  def show_response(data)
+    show_status = "#{data[:status]} #{Rack::Utils::HTTP_STATUS_CODES[data[:status]]}"
+    show_header = "[#{data[:headers]['Content-Type']}]"
+    "Response: #{show_status}, #{show_header} #{show_template(data)}"
   end
 
-  def show_template
-    return '' if @data[:controller].nil?
-    @data[:template]&.keys&.first || "#{@data[:action]}.html.erb"
-  end
-
-  def show_divider
-    '-=' * 30
+  def show_template(data)
+    return '' if data[:controller].nil?
+    data[:template].keys.first || "#{data[:action]}.html.erb"
   end
 end
